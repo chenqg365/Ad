017b7fc08fe65572384bba26565080e8951d4f92
chenqg <chenqg3721@163.com>
2020-03-11 13:49:01 +0800

    Initial Commit

---
+++ Ad/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+/*
+ ********************************************************************************
+ * File     : AppDelegate.h
+ *
+ * Author   : chenqg
+ *
+ * History  : Created by chenqg on 2020/3/11.
+ ********************************************************************************
+ */
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
